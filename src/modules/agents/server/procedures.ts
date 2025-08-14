02d235bec2df87d17c825d7148da21d52f27de3b
sandeep14032004 <sandeeppati69@gmail.com>
2025-08-14 22:09:01 +0530

    11-agents-form

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,14 +1,41 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { agentInsertSchema } from "../schemas";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-    getMany: baseProcedure.query(async () => {
+   getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+    //TODO change  "getONe" to use protectProcedure
+    const [existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id  ));
+    if (!existingAgent) {
+        throw new Error("Agent not found");
+    }
+    return existingAgent;
+    }
+    ),
+    getMany: protectedProcedure.query(async () => {
         const data = await db
             .select()
             .from(agents);
 
         
         return data;
-    })
+    }),
+    create: protectedProcedure
+    .input(agentInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+        const [createdAgent] = await db
+            .insert(agents)
+            .values({
+                ...input,
+                userId: ctx.auth.user.id,
+            })
+            .returning();
+        return createdAgent;
 })
+});
